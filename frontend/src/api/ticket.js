53cc2b5ef985bcd42160448f8f39c187a6b80d71
lucas <lucasrafaeld03@gmail.com>
2025-06-11 17:43:44 -0300

    exclusao de arquivos não utilizados

--- frontend/src/api/ticket.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
-})
-
-export const getTickets = () => api.get('/tickets')
-export const createTicket = (ticket) => api.post('/tickets', ticket)
